629e140126e0d1aab6e12576ce189a1c62a7d596
Valentino Mezzavilla <mezza12354@gmail.com>
2024-09-19 17:05:51 -0300

    save changes

---
+++ database/odontologos.js
@@ -0,0 +1,12 @@
+export const odontologos = 
+    [
+        {
+            "apellido": "mezzavilla",
+            "dni": 436916888,
+            "id": 1,
+            "nombre": "valentino"
+        }
+    ]
+;
+
+
--- javascript/pacientes.js
+++ javascript/pacientes.js
@@ -1,31 +1,51 @@
+
+//import { odontologos, odontologos } from "../database/odontologos";
 export function mainPacientes() {
-    console.log("hpña");
+    const btnHistorial = document.getElementById("btnHistorial");
+    const btnIngreso = document.getElementById("btnIngresoPaciente");
+    const forms = document.getElementById("forms");
+    const formped = document.getElementById("formPed");
+    const btnIngresoAdulto = document.getElementById("btnIngresoPacienteAdulto");
+    const btnIngresoPed = document.getElementById("btnIngresoPacientePed");
+    const historialPacientes = document.getElementById("historialPacientes");
+    const formularios = document.getElementById("formularios");
+
     document.addEventListener("DOMContentLoaded", () => {
-        const btnIngreso = document.getElementById("btnIngresoPaciente");
-        const forms = document.getElementById("forms");
-        const btnIngresoAdulto = document.getElementById("btnIngresoPacienteAdulto");
-        const btnIngresoPed = document.getElementById("btnIngresoPacientePed");
-
-        btnIngreso.addEventListener('click', () => {
-            console.log("btn");
-            
-            btnIngresoAdulto.style.visibility= 'visible';
-            btnIngresoAdulto.addEventListener('click',()=>{
-                forms.style.visibility='visible';
-                formped.style.visibility='hidden';
-                document.getElementById("hrHidden").style.visibility='visible';
-            })
-
-            btnIngresoPed.style.visibility= 'visible';
-            btnIngresoPed.addEventListener('click',()=>{
-                const formped = document.getElementById("formPed");
-                formped.style.visibility = 'visible';
-                forms.style.visibility='hidden';
-
-            })
-            
-
-        })
+        btnIngreso.addEventListener('click', ingresarPaciente());
+        btnHistorial.addEventListener('click',mostrarHistorial());
     })
+
+    function ingresarPaciente(){
+        console.log("Ingresar paciente");
+        
+            // historialPacientes.style.display='none';
+            // formularios.style.display= 'block';
+            // btnIngresoAdulto.style.visibility = 'visible';
+
+            // btnIngresoAdulto.addEventListener('click', () => {
+            //     forms.style.visibility = 'visible';
+            //     forms.style.display = 'block';
+            //     formped.style.display = 'none';
+            //     document.getElementById("hrHidden").style.visibility = 'visible';
+            // })
+
+            // btnIngresoPed.style.visibility = 'visible';
+            // btnIngresoPed.addEventListener('click', () => {
+            //     formped.style.visibility = 'visible';
+            //     forms.style.display = 'none';
+            //     formped.style.display = 'block';
+            // })
+    };
+
+    function mostrarHistorial() {
+        console.log("mostrar");
+        // btnIngresoAdulto.style.display = 'none';
+        // btnIngresoPed.style.display = 'none';
+        // forms.style.display = 'none';
+        // formped.style.display = 'none';
+        // historialPacientes.style.display = 'block';
+    }
+
+
 }
 
